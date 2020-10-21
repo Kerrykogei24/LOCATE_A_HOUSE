38efee3a0312eafa82b3ed7b5105f182602b621c
Kerrykogei24 <kerrykomar.gmail.com>
2020-10-22 01:17:45 +0300

    merged all collaborator work

--- p_js/p_script.js
+++ p_js/p_script.js
@@ -1,40 +1,40 @@
-$(document).ready(()=> {
-    $(".click1").click(()=> {
-        
-        $(".pic1-hidden").toggle();
-        $(".pic1-showing").toggle();
-    });
-    $(".click2").click(()=> {
-        $(".pic2-hidden").toggle();
-        $(".pic2-showing").toggle();
-    });
-    $(".click3").click(()=> {
-        $(".pic3-hidden").toggle();
-        $(".pic3-showing").toggle();
-    });
-    $(".click4").click(()=> {
-        $(".pic4-hidden").toggle();
-        $(".pic4-showing").toggle();
-    });
-    $(".click5").click(()=> {
-        $(".pic5-hidden").toggle();
-        $(".pic5-showing").toggle();
-    });
-    $(".click6").click(()=> {
-        $(".pic6-hidden").toggle();
-        $(".pic6-showing").toggle();
-    });
-    $(".click7").click(()=> {
-        $(".pic7-hidden").toggle();
-        $(".pic7-showing").toggle();
-    });
-    $(".click8").click(()=> {
-        $(".pic8-hidden").toggle();
-        $(".pic8-showing").toggle();
-    });
-    $(".click9").click(()=> {
-        $(".pic9-hidden").toggle();
-        $(".pic9-showing").toggle();
-    });
+// $(document).ready(()=> {
+//     $(".click1").click(()=> {
+
+//         $(".pic1-hidden").toggle();
+//         $(".pic1-showing").toggle();
+//     });
+//     $(".click2").click(()=> {
+//         $(".pic2-hidden").toggle();
+//         $(".pic2-showing").toggle();
+//     });
+//     $(".click3").click(()=> {
+//         $(".pic3-hidden").toggle();
+//         $(".pic3-showing").toggle();
+//     });
+//     $(".click4").click(()=> {
+//         $(".pic4-hidden").toggle();
+//         $(".pic4-showing").toggle();
+//     });
+//     $(".click5").click(()=> {
+//         $(".pic5-hidden").toggle();
+//         $(".pic5-showing").toggle();
+//     });
+//     $(".click6").click(()=> {
+//         $(".pic6-hidden").toggle();
+//         $(".pic6-showing").toggle();
+//     });
+//     $(".click7").click(()=> {
+//         $(".pic7-hidden").toggle();
+//         $(".pic7-showing").toggle();
+//     });
+//     $(".click8").click(()=> {
+//         $(".pic8-hidden").toggle();
+//         $(".pic8-showing").toggle();
+//     });
+//     $(".click9").click(()=> {
+//         $(".pic9-hidden").toggle();
+//         $(".pic9-showing").toggle();
+//     });
 
 });
